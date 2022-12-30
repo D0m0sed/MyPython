120333c6abdad8824e7dcc5a9b82d242874250f2
Domosed <pashik72@yandex.ru>
2022-12-30 22:40:03 +0500

    Dubble Stroks

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
 print("Hello, CHel!")
 print("Buy!")
+
+import platform
+import sys
+
+info = 'OS info is \n {}\n\n python version is {} {}'.format(platform.uname(),  sys.version, platform.architecture())
+
+print(info)

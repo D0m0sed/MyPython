e35f2412806ea6118e37cabf66cb824f342eef9d
Domosed <pashik72@yandex.ru>
2023-01-10 15:45:58 +0500

    First Commit

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
-for n in range (1, 20):
-    if n == 5:
-        continue
-    if n == 12:
-        break
-    print(n)
+import random
+
+import simple_draw as sd
+sd.resolution = (1200, 600)
+
+
+
+
+def bubble(point, step):
+    radius = 50
+    for _ in range(3):
+        sd.circle(point, radius, width=2)
+        radius += step
+
+for _ in range(100):
+
+        point = sd.random_point()
+        step = random.randint(3, 10)
+        bubble(point, step)
+
+
+sd.pause()

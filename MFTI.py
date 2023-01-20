94e153eebfead8fb73923d7c678f998e494dbe48
Domosed <pashik72@yandex.ru>
2023-01-21 01:06:19 +0500

    First MFTI Lesson_1

--- MFTI.py
+++ MFTI.py
@@ -1,11 +1,29 @@
-A = [1, 2, 3, 4, 5]
-'''for x in A:
-    #print(x, type(x))
-    x += 1
-    #print(x)
-print(A)
-for k in range(4):
-    A[k] = A[k]*A[k]
-    #print(A[k])'''
-c = list(A)
-print(c)
+def array_search(A:list, N:int, x:int):
+    for k in range(N):
+        if A[k] == x:
+            return k
+    return -1
+
+def test_AS():
+    A1 = [1, 2, 3, 4, 5]
+    m = array_search(A1, 5, 8)
+    if m==-1:
+        print("#test1 - OK")
+    else:
+        print("#test1 - fail")
+
+    A2 = [-1, -2, -3, -4, -5]
+    m = array_search(A2, 5, -3)
+    if m == 2:
+        print("#test2 - OK")
+    else:
+        print("#test2 - fail")
+
+    A3 = [10, 20, 30, 10, 10]
+    m = array_search(A3, 5, 10)
+    if m == 0:
+        print("#test3 - OK")
+    else:
+        print("#test3 - fail")
+
+test_AS()
